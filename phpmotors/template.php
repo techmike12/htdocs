fa06c414c98b201d61b8aa8f0df0dcf60de5b29e
techmike12 <mkendall32@gmail.com>
2020-09-21 10:21:50 -0600

    Template

---
+++ phpmotors/template.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Michael Kendall">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name= "description" content="PHP Motors">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/small.css">
+    <link rel="stylesheet" href="css/medium.css">
+    <link rel="stylesheet" href="css/large.css">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <title>PHP Motors</title>
+</head>
+<body id="background">
+    <div id="content">
+    <header>
+        <img id="logo" src="/phpmotors/images/site/logo.png" alt="PHP Logo">
+        <h2 id="account"><a href="#">My Account</a></h2>
+    </header>
+    <nav>
+        <ul class="navigation">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Classics</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="#">SUV</a></li>
+            <li><a href="#">Trucks</a></li>
+            <li><a href="#">Used</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h1>Content Title here</h1>
+    </main>
+    <hr id="break">
+    <footer>
+        <div>&copy; PHP Motors, All rights reserved.</div>
+        <span> All images used are believed to be in "Fair Use". Please notify the author if any are no and they will be removed</span>
+        <span id="modified"></span>
+    </footer>
+    </div>
+    <script>
+    </script>
+</body>
+</html>
