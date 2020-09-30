e9d64645e05699b909df302fb24f0a47581b6037
techmike12 <mkendall32@gmail.com>
2020-09-30 07:57:28 -0600

    Updated based on Feedback

--- phpmotors/home.php
+++ phpmotors/home.php
@@ -5,10 +5,10 @@
     <meta name="author" content="Michael Kendall">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="PHP Motors">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/small.css">
-    <link rel="stylesheet" href="css/medium.css">
-    <link rel="stylesheet" href="css/large.css">
+    <link rel="stylesheet" href="css/normalize.css" media="screen">
+    <link rel="stylesheet" href="css/small.css" media="screen">
+    <link rel="stylesheet" href="css/medium.css" media="screen">
+    <link rel="stylesheet" href="css/large.css" media="screen">
     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
     <title>PHP Motors</title>
 </head>
@@ -28,7 +28,7 @@
                 <p>3 Cup holders</p>
                 <p>Superman doors</p>
                 <p>Fuzzy dice!</p>
-                <input type="image" src="/phpmotors/images/site/own_today.png">
+                <input type="image" src="/phpmotors/images/site/own_today.png" alt="Own Today Button">
             </div>
             <div id="dmc">
                 <img id="delor" src="/phpmotors/images/delorean.jpg" alt="DMC Delorean">
