4494813da33d518c61136de5e2977af922f6ee73
techmike12 <mkendall32@gmail.com>
2020-11-30 13:40:53 -0700

    Updated with Account Link

--- phpmotors/common/header.php
+++ phpmotors/common/header.php
@@ -1,7 +1,7 @@
     <img id="logo" src="/phpmotors/images/site/logo.png" alt="PHP Logo">
     <?php if($_SESSION){
         $client = $_SESSION['clientData']['clientFirstname'];
-        echo "<span id='welcome'>Welcome $client</span>";
+        echo "<a id='welcome' href='/phpmotors/accounts/'>Welcome $client</span>";
     }     if (!$_SESSION) {
         echo '<a id="account" href="/phpmotors/accounts/index.php?action=login">My Account</a>';
     } else {echo '<a id="account" href="/phpmotors/accounts/index.php?action=logout">Logout</a>';}?>
